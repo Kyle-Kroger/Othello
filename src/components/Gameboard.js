316bf912c24520e841129f12016a7e795f987f85
Kyle Kroger <kyle.s.kroger@gmail.com>
2022-06-11 22:30:41 -0500

    refactor game logic and init state

--- src/components/Gameboard.js
+++ src/components/Gameboard.js
@@ -1,134 +1,21 @@
 import { useState, useEffect, useCallback } from "react";
 import styled from "styled-components";
 import Gamesquare from "./Gamesquare";
-
-//move this to its own file
-const INITIAL_BOARD = [
-  [
-    { state: "e", possibleMove: false },
-    { state: "e", possibleMove: false },
-    { state: "e", possibleMove: false },
-    { state: "e", possibleMove: false },
-    { state: "e", possibleMove: false },
-    { state: "e", possibleMove: false },
-    { state: "e", possibleMove: false },
-    { state: "e", possibleMove: false },
-  ],
-  [
-    { state: "e", possibleMove: false },
-    { state: "e", possibleMove: false },
-    { state: "e", possibleMove: false },
-    { state: "e", possibleMove: false },
-    { state: "e", possibleMove: false },
-    { state: "e", possibleMove: false },
-    { state: "e", possibleMove: false },
-    { state: "e", possibleMove: false },
-  ],
-  [
-    { state: "e", possibleMove: false },
-    { state: "e", possibleMove: false },
-    { state: "e", possibleMove: false },
-    { state: "e", possibleMove: false },
-    { state: "e", possibleMove: false },
-    { state: "e", possibleMove: false },
-    { state: "e", possibleMove: false },
-    { state: "e", possibleMove: false },
-  ],
-  [
-    { state: "e", possibleMove: false },
-    { state: "e", possibleMove: false },
-    { state: "e", possibleMove: false },
-    { state: "w", possibleMove: false },
-    { state: "b", possibleMove: false },
-    { state: "e", possibleMove: false },
-    { state: "e", possibleMove: false },
-    { state: "e", possibleMove: false },
-  ],
-  [
-    { state: "e", possibleMove: false },
-    { state: "e", possibleMove: false },
-    { state: "e", possibleMove: false },
-    { state: "b", possibleMove: false },
-    { state: "w", possibleMove: false },
-    { state: "e", possibleMove: false },
-    { state: "e", possibleMove: false },
-    { state: "e", possibleMove: false },
-  ],
-  [
-    { state: "e", possibleMove: false },
-    { state: "e", possibleMove: false },
-    { state: "e", possibleMove: false },
-    { state: "e", possibleMove: false },
-    { state: "e", possibleMove: false },
-    { state: "e", possibleMove: false },
-    { state: "e", possibleMove: false },
-    { state: "e", possibleMove: false },
-  ],
-  [
-    { state: "e", possibleMove: false },
-    { state: "e", possibleMove: false },
-    { state: "e", possibleMove: false },
-    { state: "e", possibleMove: false },
-    { state: "e", possibleMove: false },
-    { state: "e", possibleMove: false },
-    { state: "e", possibleMove: false },
-    { state: "e", possibleMove: false },
-  ],
-  [
-    { state: "e", possibleMove: false },
-    { state: "e", possibleMove: false },
-    { state: "e", possibleMove: false },
-    { state: "e", possibleMove: false },
-    { state: "e", possibleMove: false },
-    { state: "e", possibleMove: false },
-    { state: "e", possibleMove: false },
-    { state: "e", possibleMove: false },
-  ],
-];
+import { validMoves } from "../lib/gameLogic";
+import INITIAL_BOARD from "../lib/initialState";
 
 const Gameboard = () => {
   const [gameState, setGameState] = useState(INITIAL_BOARD);
   const [player, setPlayer] = useState("b");
   //create a game log that explains what happened on each turn
 
-  const validMoves = useCallback((gameboard, color) => {
-    const newGameboard = [...gameboard].map((row, rowI) => {
-      return row.map((col, colI) => {
-        if (col.state === "e") {
-          const surroundingOppositePieces = checkAdjacentSquares(
-            gameboard,
-            color,
-            rowI,
-            colI
-          );
-          if (surroundingOppositePieces.length > 0) {
-            //console.log(rowI, colI, surroundingOppositePieces);
-            let toFlip = validateMove(
-              gameboard,
-              color,
-              rowI,
-              colI,
-              surroundingOppositePieces
-            );
-            if (toFlip.length > 0) {
-              //console.log(rowI, colI, toFlip);
-              //add valid into the gamestate
-              return { ...col, possibleMove: true, toFlip };
-            }
-          }
-        }
-
-        return { ...col, possibleMove: false, toFlip: undefined };
-      });
-    });
-
-    setGameState(newGameboard);
-    console.log(newGameboard);
+  const memoValidMoves = useCallback((gameboard, color) => {
+    return validMoves(gameboard, color);
   }, []);
 
   useEffect(() => {
-    validMoves(gameState, player);
-  }, [validMoves, gameState, player]);
+    setGameState((prevState) => memoValidMoves(prevState, player));
+  }, [memoValidMoves, player]);
 
   const placePiece = (x, y, color) => {
     setGameState((prevState) => {
@@ -140,70 +27,6 @@ const Gameboard = () => {
     setPlayer((prevState) => {
       return prevState === "w" ? "b" : "w";
     });
-  };
-
-  const validateMove = (gameboard, color, x, y, oppAdjacentArr) => {
-    const opposite = color === "w" ? "b" : "w";
-    for (let oppAdjacent of oppAdjacentArr) {
-      let { x: currX, y: currY } = oppAdjacent;
-      const xDir = oppAdjacent.x - x;
-      const yDir = oppAdjacent.y - y;
-      let validated = [];
-      let valid = false;
-
-      while (
-        currX >= 0 &&
-        currX < gameboard.length &&
-        currY >= 0 &&
-        currY < gameboard.length
-      ) {
-        if (gameboard[currX][currY].state === opposite) {
-          validated.push({ x: currX, y: currY });
-        }
-        if (gameboard[currX][currY].state === color) {
-          valid = validated.length > 0;
-          break;
-        }
-
-        currX += xDir;
-        currY += yDir;
-      }
-
-      if (valid) {
-        return validated;
-      }
-
-      return [];
-    }
-  };
-
-  const checkAdjacentSquares = (gameboard, color, x, y) => {
-    const opposite = color === "w" ? "b" : "w";
-    const surrounding = [
-      { x: x - 1, y: y - 1 },
-      { x: x - 1, y },
-      { x: x - 1, y: y + 1 },
-      { x, y: y + 1 },
-      { x: x + 1, y: y + 1 },
-      { x: x + 1, y },
-      { x: x + 1, y: y - 1 },
-      { x, y: y - 1 },
-    ];
-    const oppositePieces = [];
-    for (let square of surrounding) {
-      if (
-        square.x < 0 ||
-        square.y < 0 ||
-        square.x >= gameboard.length ||
-        square.y >= gameboard[0].length
-      ) {
-        continue;
-      }
-      if (gameboard[square.x][square.y].state === opposite) {
-        oppositePieces.push({ x: square.x, y: square.y });
-      }
-    }
-    return oppositePieces;
   };
 
   const flipPieces = (gameboard, arrPieces) => {};
@@ -228,7 +51,7 @@ const Gameboard = () => {
       </StyledBoard>
       <button
         onClick={() => {
-          validMoves(gameState, player);
+          memoValidMoves(gameState, player);
         }}
       >
         Test
--- src/lib/gameLogic.js
+++ src/lib/gameLogic.js
@@ -1,3 +1,191 @@
-const gameLogic = {};
+export const validMoves = (gameboard, color) => {
+  console.log(gameboard, color);
+  const newGameboard = [...gameboard].map((row, rowI) => {
+    return row.map((col, colI) => {
+      if (col.state === "e") {
+        const surroundingOppositePieces = checkAdjacentSquares(
+          gameboard,
+          color,
+          rowI,
+          colI
+        );
+        if (surroundingOppositePieces.length > 0) {
+          let toFlip = validateMove(
+            gameboard,
+            color,
+            rowI,
+            colI,
+            surroundingOppositePieces
+          );
+          if (toFlip.length > 0) {
+            return { ...col, possibleMove: true, toFlip };
+          }
+        }
+      }
 
-export default gameLogic;
+      return { ...col, possibleMove: false, toFlip: undefined };
+    });
+  });
+
+  console.log(newGameboard);
+  return newGameboard;
+};
+
+const validateMove = (gameboard, color, x, y, oppAdjacentArr) => {
+  const opposite = color === "w" ? "b" : "w";
+  for (let oppAdjacent of oppAdjacentArr) {
+    let { x: currX, y: currY } = oppAdjacent;
+    const xDir = oppAdjacent.x - x;
+    const yDir = oppAdjacent.y - y;
+    let validated = [];
+    let valid = false;
+
+    while (
+      currX >= 0 &&
+      currX < gameboard.length &&
+      currY >= 0 &&
+      currY < gameboard.length
+    ) {
+      if (gameboard[currX][currY].state === opposite) {
+        validated.push({ x: currX, y: currY });
+      }
+      if (gameboard[currX][currY].state === color) {
+        valid = validated.length > 0;
+        break;
+      }
+
+      currX += xDir;
+      currY += yDir;
+    }
+
+    if (valid) {
+      return validated;
+    }
+
+    return [];
+  }
+};
+
+const checkAdjacentSquares = (gameboard, color, x, y) => {
+  const opposite = color === "w" ? "b" : "w";
+  const surrounding = [
+    { x: x - 1, y: y - 1 },
+    { x: x - 1, y },
+    { x: x - 1, y: y + 1 },
+    { x, y: y + 1 },
+    { x: x + 1, y: y + 1 },
+    { x: x + 1, y },
+    { x: x + 1, y: y - 1 },
+    { x, y: y - 1 },
+  ];
+  const oppositePieces = [];
+  for (let square of surrounding) {
+    if (
+      square.x < 0 ||
+      square.y < 0 ||
+      square.x >= gameboard.length ||
+      square.y >= gameboard[0].length
+    ) {
+      continue;
+    }
+    if (gameboard[square.x][square.y].state === opposite) {
+      oppositePieces.push({ x: square.x, y: square.y });
+    }
+  }
+  return oppositePieces;
+};
+
+// const gameLogic = {
+//   validMoves: (gameboard, color) => {
+//     const newGameboard = [...gameboard].map((row, rowI) => {
+//       return row.map((col, colI) => {
+//         if (col.state === "e") {
+//           const surroundingOppositePieces = this.checkAdjacentSquares(
+//             gameboard,
+//             color,
+//             rowI,
+//             colI
+//           );
+//           if (surroundingOppositePieces.length > 0) {
+//             let toFlip = this.validateMove(
+//               gameboard,
+//               color,
+//               rowI,
+//               colI,
+//               surroundingOppositePieces
+//             );
+//             if (toFlip.length > 0) {
+//               return { ...col, possibleMove: true, toFlip };
+//             }
+//           }
+//         }
+
+//         return { ...col, possibleMove: false, toFlip: undefined };
+//       });
+//     });
+
+//     return newGameboard;
+//   },
+//   validateMove: (gameboard, color, x, y, oppAdjacentArr) => {
+//     const opposite = color === "w" ? "b" : "w";
+//     for (let oppAdjacent of oppAdjacentArr) {
+//       let { x: currX, y: currY } = oppAdjacent;
+//       const xDir = oppAdjacent.x - x;
+//       const yDir = oppAdjacent.y - y;
+//       let validated = [];
+//       let valid = false;
+
+//       while (
+//         currX >= 0 &&
+//         currX < gameboard.length &&
+//         currY >= 0 &&
+//         currY < gameboard.length
+//       ) {
+//         if (gameboard[currX][currY].state === opposite) {
+//           validated.push({ x: currX, y: currY });
+//         }
+//         if (gameboard[currX][currY].state === color) {
+//           valid = validated.length > 0;
+//           break;
+//         }
+
+//         currX += xDir;
+//         currY += yDir;
+//       }
+
+//       if (valid) {
+//         return validated;
+//       }
+
+//       return [];
+//     }
+//   },
+//   checkAdjacentSquares: (gameboard, color, x, y) => {
+//     const opposite = color === "w" ? "b" : "w";
+//     const surrounding = [
+//       { x: x - 1, y: y - 1 },
+//       { x: x - 1, y },
+//       { x: x - 1, y: y + 1 },
+//       { x, y: y + 1 },
+//       { x: x + 1, y: y + 1 },
+//       { x: x + 1, y },
+//       { x: x + 1, y: y - 1 },
+//       { x, y: y - 1 },
+//     ];
+//     const oppositePieces = [];
+//     for (let square of surrounding) {
+//       if (
+//         square.x < 0 ||
+//         square.y < 0 ||
+//         square.x >= gameboard.length ||
+//         square.y >= gameboard[0].length
+//       ) {
+//         continue;
+//       }
+//       if (gameboard[square.x][square.y].state === opposite) {
+//         oppositePieces.push({ x: square.x, y: square.y });
+//       }
+//     }
+//     return oppositePieces;
+//   },
+// };
